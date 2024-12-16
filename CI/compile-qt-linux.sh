3a3bd191c4c2a329205624977805e1df14ceacb2
John McKisson <john.mckisson@gmail.com>
2024-12-16 17:19:38 -0500

    try just not using qttools

--- CI/compile-qt-linux.sh
+++ CI/compile-qt-linux.sh
@@ -4,16 +4,16 @@ echo "=== Cloning Qt Source Repository ==="
 cd ${RUNNER_WORKSPACE}
 git clone --branch 6.8.1 --depth 1 --no-recurse-submodules https://github.com/qt/qt5.git qt6-source
 cd qt6-source
-git submodule update --init qtbase qttools qttranslations
+git submodule update --init qtbase qttranslations
 
 echo "=== Configuring Qt for Static Linking ==="
-perl init-repository --module-subset=qtbase,qttools,qttranslations
+perl init-repository --module-subset=qtbase,qttranslations
 cd ${RUNNER_WORKSPACE}
 mkdir qt-static-build
 cd qt-static-build
 export CMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON
 
-../qt6-source/configure -prefix ${RUNNER_WORKSPACE}/qt-static-install -no-feature-tracepoint -static -release -opensource -confirm-license -init-submodules -submodules qtbase,qttranslations,qttools -nomake tests -nomake examples -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtactiveqt  -skip qtshadertools -skip qtquick -skip designer -no-opengl -no-dbus -platform linux-g++ -openssl-linked -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12
+../qt6-source/configure -prefix ${RUNNER_WORKSPACE}/qt-static-install -static -release -opensource -confirm-license -init-submodules -submodules qtbase,qttranslations -nomake tests -nomake examples -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtactiveqt  -skip qtshadertools -skip qtquick -skip designer -no-opengl -no-dbus -platform linux-g++ -openssl-linked -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12
 
 # CMake configuration with ccache integration
 #cmake -DCMAKE_BUILD_TYPE=Release \
@@ -27,8 +27,6 @@ export CMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON
 
 echo "=== Compiling Qt ==="
 cmake --build . --parallel
-
-cmake --build . --target tracepointgen --parallel
 
 echo "=== Installing Qt ==="
 cmake --install .
