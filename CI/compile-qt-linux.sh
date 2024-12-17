8b1bad383a757bb6e54fd56ed214596575306c7a
John McKisson <john.mckisson@gmail.com>
2024-12-16 20:23:03 -0500

    try more stuff

--- CI/compile-qt-linux.sh
+++ CI/compile-qt-linux.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "=== Cloning Qt Source Repository ==="
-cd ${RUNNER_WORKSPACE}
+cd ${GITHUB_WORKSPACE}
 git clone --branch 6.8.1 --depth 1 --no-recurse-submodules https://github.com/qt/qt5.git qt6-source
 cd qt6-source
 git submodule update --init qtbase qttranslations
 
 echo "=== Configuring Qt for Static Linking ==="
 #perl init-repository --module-subset=qtbase,qttranslations
-cd ${RUNNER_WORKSPACE}
+cd ${GITHUB_WORKSPACE}
 mkdir qt-static-build
 cd qt-static-build
 export CMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON
 
-../qt6-source/configure -prefix ${RUNNER_WORKSPACE}/qt-static-install -static -release -opensource -confirm-license -init-submodules -submodules qtbase,qttranslations -nomake tests -nomake examples -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtactiveqt  -skip qtshadertools -skip qtquick -skip designer -no-opengl -no-dbus -platform linux-g++ -openssl-linked -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12
+../qt6-source/configure -prefix ${GITHUB_WORKSPACE}/qt-static-install -static -release -opensource -confirm-license -init-submodules -submodules qtbase,qttranslations -nomake tests -nomake examples -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtactiveqt  -skip qtshadertools -skip qtquick -skip designer -no-opengl -no-dbus -platform linux-g++ -openssl-linked -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12
 
 # CMake configuration with ccache integration
 #cmake -DCMAKE_BUILD_TYPE=Release \
