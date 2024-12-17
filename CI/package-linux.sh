cbfed93fd84745bfa7a1e85ef8f05cc2e20a045e
John McKisson <john.mckisson@gmail.com>
2024-12-17 16:01:11 -0500

    add linux package script, revert to non-static qt for linux and macos

---
+++ CI/package-linux.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# set -e
+set -x
+
+BUILD_DIR="${BUILD_FOLDER}"
+SOURCE_DIR="${GITHUB_WORKSPACE}"
+
+ln -s "${BUILD_DIR}" source
+
+# unset LD_LIBRARY_PATH as it upsets linuxdeployqt
+export LD_LIBRARY_PATH=
+
+echo "== Creating an AppImage =="
+
+# setup linuxdeployqt binary if not found
+if [ "$(getconf LONG_BIT)" = "64" ]; then
+  if [[ ! -e linuxdeployqt.AppImage ]]; then
+      # download prepackaged linuxdeployqt. Doesn't seem to have a "latest" url yet
+      echo "linuxdeployqt not found - downloading one."
+      wget --quiet -O linuxdeployqt.AppImage https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage
+      chmod +x linuxdeployqt.AppImage
+  fi
+else
+  echo "32bit Linux is currently not supported by the AppImage."
+  exit 2
+fi
+
+./linuxdeployqt.AppImage --appimage-extract
+
+# Bundle libssl.so so Mudlet works on platforms that only distribute
+# OpenSSL 1.1
+cp -L /usr/lib/x86_64-linux-gnu/libssl.so* \
+      build/lib/ || true
+cp -L /lib/x86_64-linux-gnu/libssl.so* \
+      build/lib/ || true
+if [ -z "$(ls build/lib/libssl.so*)" ]; then
+  echo "No OpenSSL libraries to copy found. Aborting..."
+  exit 1
+fi
+
+./squashfs-root/AppRun ./build/MudletBootstrap -appimage \
+  -executable=build/lib/libssl.so.1.1 \
+  -executable=build/lib/libssl.so.1.0.0
+
+chmod +x "MudletBootstrap.AppImage"
+tar -cvf "MudletBootstrap-linux-x64.AppImage.tar" "MudletBootstrap.AppImage"
+
+echo "=== ... later, via Github ==="
+# Move the finished file into a folder of its own, because we ask Github to upload contents of a folder
+mkdir "upload/"
+mv "MudletBootstrap-linux-x64.AppImage.tar" "upload/"
+{
+  echo "FOLDER_TO_UPLOAD=$(pwd)/upload"
+  echo "UPLOAD_FILENAME=MudletBootstrap-linux-x64"
+} >> "$GITHUB_ENV"
+DEPLOY_URL="Github artifact, see https://github.com/$GITHUB_REPOSITORY/runs/$GITHUB_RUN_ID"
+
+export DEPLOY_URL
+
