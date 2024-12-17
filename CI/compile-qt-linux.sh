7fdc5943e66fda594c2e459ccac615e82ee5c548
John McKisson <john.mckisson@gmail.com>
2024-12-17 09:47:51 -0500

    qt build doesnt work up one directory

--- CI/compile-qt-linux.sh
+++ CI/compile-qt-linux.sh
@@ -8,12 +8,12 @@ git submodule update --init qtbase qttranslations
 
 echo "=== Configuring Qt for Static Linking ==="
 #perl init-repository --module-subset=qtbase,qttranslations
-cd ${GITHUB_WORKSPACE}/..
+cd ${GITHUB_WORKSPACE}
 mkdir qt-static-build
 cd qt-static-build
 export CMAKE_SUPPRESS_DEVELOPER_WARNINGS=ON
 
-${GITHUB_WORKSPACE}/qt6-source/configure -prefix ${GITHUB_WORKSPACE}/../qt-static-install -static -release -opensource -confirm-license -init-submodules -submodules qtbase,qttranslations -nomake tests -nomake examples -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtactiveqt  -skip qtshadertools -skip qtquick -skip designer -no-opengl -no-dbus -platform linux-g++ -openssl-linked -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12
+${GITHUB_WORKSPACE}/qt6-source/configure -prefix ${GITHUB_WORKSPACE}/qt-static-install -static -release -opensource -confirm-license -init-submodules -submodules qtbase,qttranslations -nomake tests -nomake examples -skip qt3d -skip qtmultimedia -skip qtdeclarative -skip qtactiveqt  -skip qtshadertools -skip qtquick -skip designer -no-opengl -no-dbus -platform linux-g++ -openssl-linked -DCMAKE_PREFIX_PATH=/usr/lib/llvm-12
 
 echo "=== Compiling Qt ==="
 cmake --build . -- -j2
