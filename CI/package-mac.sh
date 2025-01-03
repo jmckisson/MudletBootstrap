5286685366fa68bf860456108b7e96be80182e74
John McKisson <john.mckisson@gmail.com>
2025-01-03 10:29:56 -0500

    fix icon location

--- CI/package-mac.sh
+++ CI/package-mac.sh
@@ -138,7 +138,7 @@ echo "Modifying config file..."
 # Modify appdmg config file according to the app file to package
 perl -pi -e "s|../source/build/.*MudletBootstrap.*\\.app|${BUILD_DIR}/${app}|i" "${SOURCE_DIR}/mudletbootstrap-appdmg.json"
 # Update icons to the correct type
-perl -pi -e "s|../source/src/icons/.*\\.icns|${SOURCE_DIR}/src/icons/mudlet.icns|i" "${SOURCE_DIR}/mudletbootstrap-appdmg.json"
+perl -pi -e "s|../source/src/icons/.*\\.icns|${SOURCE_DIR}/mudlet.icns|i" "${SOURCE_DIR}/mudletbootstrap-appdmg.json"
 
 echo "Listing config file:"
 cat ${SOURCE_DIR}/mudletbootstrap-appdmg.json
